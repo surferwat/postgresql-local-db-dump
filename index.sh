03715df530f15e14549cd6385b1c426e27c17a97
surferwat <wmiura@gmail.com>
2021-09-20 17:39:55 +0900

    fix: clean up

--- index.sh
+++ index.sh
@@ -5,63 +5,39 @@ DUMP_TO_PATH=$2
 ARGS=2
 CURRENT_DATE=`date +"%Y-%m-%d"`
 
-# Check pre-conditions
-if [ $# -ne $ARGS ] 
-then
-  echo "invalid number of args"
+# Function to check pre- and post-conditions
+check_condition() {
+ if [ $1 ] 
+ then 
+  echo $2
   echo "...exiting"
   exit 1
-fi
+ fi
+}
 
-if [ ! -d $DUMP_TO_PATH ]
-then
- echo "$DUMP_TO_PATH does not exist"
- echo "...exiting"
- exit 1
-fi
+# Check pre-conditions
+check_condition "$# -ne $ARGS" "invalid number of args" 
+check_condition "! -d $DUMP_TO_PATH" "$DUMP_TO_PATH does not exist" 
 
 # Dump data for production db in script format
 echo "...creating data dump of production db"
 sudo -u postgres pg_dump -O -x --cluster 13/main $PRODUCTION_DB_URL > ${DUMP_TO_PATH}/production_dump.sql
-
-if [ $? -ne 0 ]
-then 
- echo "could not create data dump of production db"
- echo "...exiting"
- exit 1
-fi
+check_condition "$? -ne 0" "could not create data dump of production db" 
 
 # Create local db
 echo "...creating local db"
 sudo -u postgres createdb ${CURRENT_DATE}-localdb
-
-if [ $? -ne 0 ]
-then
- echo "could not create local db"
- echo "...exiting"
- exit 1
-fi
+check_condition "$? -ne 0" "could not create local db"
 
 # Restore to local db
 echo "...restoring data dump to local db"
 sudo -u postgres psql ${CURRENT_DATE}-localdb < ${DUMP_TO_PATH}/production_dump.sql > /dev/null
-
-if [ $? -ne 0 ]
-then 
- echo "could not restore data dump to local db"
- echo "...exiting"
- exit 1
-fi
+check_condition "$? -ne 0" "could not restore data dump to local db"
 
 # Dump data for local db in script format
 echo "...creating data dump of local db"
 sudo -u postgres pg_dump -O -x ${CURRENT_DATE}-localdb > ${DUMP_TO_PATH}/local_dump.sql
-
-if [ $? -ne 0 ]
-then 
- echo "could not create data dump of local db"
- echo "...exiting"
- exit 1
-fi
+check_condition "$? -ne 0" "could not create data dump of local db"
 
 echo "done"
+exit 0
